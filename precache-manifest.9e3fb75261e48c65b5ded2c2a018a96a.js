e6ae05e2c49d45c631c26951747535e94aafd476
Tara Yoo <yootar@gmail.com>
2019-10-01 19:55:44 -0400

    Updates

---
+++ precache-manifest.9e3fb75261e48c65b5ded2c2a018a96a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9151dcd2f1fc2d2063425191831e50",
+    "url": "/TarotFrontEnd/index.html"
+  },
+  {
+    "revision": "a0ce9315c0df0105ecc1",
+    "url": "/TarotFrontEnd/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "9837f9d3451c6ebab789",
+    "url": "/TarotFrontEnd/static/js/2.4dae6e8e.chunk.js"
+  },
+  {
+    "revision": "a0ce9315c0df0105ecc1",
+    "url": "/TarotFrontEnd/static/js/main.01ac17de.chunk.js"
+  },
+  {
+    "revision": "47de27bf95a2db05ea58",
+    "url": "/TarotFrontEnd/static/js/runtime-main.6a6a9f6f.js"
+  }
+]);
